76c29aefdd7eab4bfae8cb977e648edcd37cbd72
Abdubek <zholay.abdubek@gmail.com>
2025-11-21 19:32:34 +0500

    add linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,7 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettierConfig from "eslint-config-prettier";
 import prettierPlugin from "eslint-plugin-prettier";
-import stylisticJs from "@stylistic/eslint-plugin-js";
-import stylisticTs from "@stylistic/eslint-plugin-ts";
+import stylistic from "@stylistic/eslint-plugin";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -16,27 +15,29 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    ".history/**",
   ]),
   {
     plugins: {
-      "@stylistic/js": stylisticJs,
-      "@stylistic/ts": stylisticTs,
+      "@stylistic": stylistic,
       prettier: prettierPlugin,
     },
     rules: {
       // Prettier integration
       "prettier/prettier": "error",
-      
+
       // Stylistic rules
-      "@stylistic/js/indent": ["error", 2],
-      "@stylistic/js/quotes": ["error", "single"],
-      "@stylistic/js/semi": ["error", "always"],
-      "@stylistic/ts/indent": ["error", 2],
-      "@stylistic/ts/quotes": ["error", "single"],
-      "@stylistic/ts/semi": ["error", "always"],
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/semi": ["error", "always"],
+      "@stylistic/comma-dangle": ["error", "es5"],
+      "@stylistic/arrow-parens": ["error", "always"],
     },
   },
   prettierConfig,
+  {
+    extends: ["prettier"],
+  },
 ]);
 
 export default eslintConfig;
