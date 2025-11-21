9fbdd73d800555ab4df9e5c6f2cd1bff5e382f8b
Abdubek <zholay.abdubek@gmail.com>
2025-11-22 02:29:12 +0500

    fix signIn create function

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -2,22 +2,32 @@
 
 import { Button } from "@/src/components/ui/button";
 import { useSignIn, useUser } from "@clerk/nextjs";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function AuthPage() {
-  const { signIn } = useSignIn();
+  const { signIn, setActive } = useSignIn();
   const { isLoaded } = useUser();
   const searchParams = useSearchParams();
   const redirectUrl = searchParams.get("redirect_url");
+  const router = useRouter();
 
   console.log(redirectUrl);
 
   const handleSignIn = async () => {
-    await signIn?.create({
-      strategy: "oauth_google",
-      redirectUrl: "/auth/sso-callback",
-      actionCompleteRedirectUrl: redirectUrl || "/",
-    });
+    try {
+      const result = await signIn?.create({
+        strategy: "oauth_google",
+        redirectUrl: "/auth/sso-callback",
+        actionCompleteRedirectUrl: redirectUrl || "/",
+      });
+
+      if (result?.status === "complete") {
+        await setActive?.({ session: result.createdSessionId });
+        router.push("/dashboard");
+      }
+    } catch (err) {
+      console.error("Error:", err);
+    }
   };
 
   if (!signIn || !isLoaded) return null;
